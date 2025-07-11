c21318ae550460adf275669a3ac1a1ae886e4a7b
maryamarg <maryamabubakar0903646@gmail.com>
2025-07-11 19:41:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-wDjHUJy8hsBzygHt',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348088629322",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "[.]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
